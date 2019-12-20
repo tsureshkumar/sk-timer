81865ed4ffd94c4511750e6909971e3d9b2fb9a1
Sureshkumar T <tsureshkumar2000@gmail.com>
2019-12-21 03:43:44 +0530

    simple timer

--- src/index.ts
+++ src/index.ts
@@ -14,91 +14,42 @@ const dateFormat = (counter: number) => {
 
 const updateTimer = (elapsed: number) => $('#timer').html(dateFormat(elapsed));
 
-interface Counter {
-  count: number;
-  inc: number; // in millis
-}
-
-interface Control {
-  play: boolean;
-  stop: boolean;
-}
-
 const enum Command {
   Play,
   Pause,
   Stop,
-  Inc,
   Reset
 }
 
-const play$ = rx
-  .fromEvent(document.getElementById('play'), 'click')
-  .pipe(rxop.mapTo(Command.Play));
-const stop$ = rx
-  .fromEvent(document.getElementById('stop'), 'click')
-  .pipe(rxop.mapTo(Command.Stop));
-const reset$ = rx
-  .fromEvent(document.getElementById('reset'), 'click')
-  .pipe(rxop.mapTo(Command.Reset));
-const control$ = rx.merge(
-  rx.merge(play$, stop$, reset$).pipe(
-    rxop.scan((prev: Command, curr: Command) => {
-      switch (curr) {
-        case Command.Stop:
-          return Command.Stop;
-        case Command.Play:
-          if (prev === Command.Stop) return Command.Play;
-          if (prev === Command.Play) return Command.Pause;
-          if (prev === Command.Reset) return Command.Stop;
-          return Command.Play;
-        default:
-          return Command.Stop;
-      }
-    }, Command.Pause),
-    rxop.tap((cmd: Command) => {
-      switch (cmd) {
-        case Command.Play:
-          $('#play').html("<i class='fas fa-pause fa-lg'></i>");
-          break;
-        case Command.Pause:
-          $('#play').html("<i class='fas fa-play fa-lg'></i>");
-          break;
-        case Command.Stop:
-          $('#play').html("<i class='fas fa-play fa-lg'></i>");
-          break;
-        default:
-      }
-    })
-  ),
-  reset$
+interface Counter {
+  count: number;
+}
+
+const fromEventMapTo = (id: string, cmd: Command) =>
+  rx.fromEvent($(id), 'click').pipe(rxop.mapTo(cmd));
+const command$ = rx.merge(
+  fromEventMapTo('#play', Command.Play),
+  fromEventMapTo('#stop', Command.Stop),
+  fromEventMapTo('#reset', Command.Reset)
 );
 
 const delta = 5;
-const switcher$ = control$.pipe(
-  rxop.tap((cmd: Command) => console.log(cmd)),
-  rxop.switchMap((cmd: Command) =>
-    cmd === Command.Play
-      ? rx.interval(delta).pipe(rxop.mapTo(Command.Inc))
-      : rx.NEVER
-  )
-);
-
-const stopWatch$ = rx.merge(control$, switcher$).pipe(
-  rxop.filter((v: Command) => v === Command.Reset || v === Command.Inc),
+const counter$ = command$.pipe(
+  rxop.startWith(Command.Reset),
+  rxop.switchMap((cmd: Command) => {
+    if (cmd === Command.Play)
+      return rx.interval(delta).pipe(rxop.map(x => [x, cmd]));
+    if (cmd === Command.Reset) return rx.of([0, cmd]);
+    return rx.NEVER;
+  }),
   rxop.scan(
-    (acc: Counter, curr: Command) => {
-      console.log(acc, curr);
-      return {
-        ...acc,
-        count: curr === Command.Reset ? 0 : acc.count + acc.inc
-      };
-    },
-    { count: 0, inc: delta }
-  )
+    (prev: Counter, [_, cmd]: [number, Command]) => ({
+      ...prev,
+      count: cmd === Command.Reset ? 0 : prev.count + delta
+    }),
+    { count: 0 }
+  ),
+  rxop.map(x => x.count)
 );
 
-stopWatch$.subscribe((value: Counter) => {
-  console.log('fired', value);
-  updateTimer(value.count);
-});
+counter$.subscribe((x: any) => updateTimer(x));
